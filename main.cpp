bbfe716b9170d291bb3d383ec1afea72d89da52f
rafavferraz <rafavferraz@gmail.com>
2022-05-17 16:17:24 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include "../../utility/print.h"
 
-// #include "dependencies/glfw-3.3.7/include/GLFW/glfw3.h"
-// #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-
 int main() {
-
-
 
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -15,48 +10,38 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+  GLFWwindow* window;
 
+  /* Initialize the library */
+  if (!glfwInit())
+      return -1;
 
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window)
+  {
+      glfwTerminate();
+      return -1;
+  }
 
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
 
-  // GLFWwindow* window;
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window))
+  {
+      /* Render here */
+      glClear(GL_COLOR_BUFFER_BIT);
 
-  // /* Initialize the library */
-  // if (!glfwInit())
-  //     return -1;
+      /* Swap front and back buffers */
+      glfwSwapBuffers(window);
 
-  // /* Create a windowed mode window and its OpenGL context */
-  // window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  // if (!window)
-  // {
-  //     glfwTerminate();
-  //     return -1;
-  // }
+      /* Poll for and process events */
+      glfwPollEvents();
+  }
 
-  // /* Make the window's context current */
-  // glfwMakeContextCurrent(window);
-
-  // /* Loop until the user closes the window */
-  // while (!glfwWindowShouldClose(window))
-  // {
-  //     /* Render here */
-  //     glClear(GL_COLOR_BUFFER_BIT);
-
-  //     /* Swap front and back buffers */
-  //     glfwSwapBuffers(window);
-
-  //     /* Poll for and process events */
-  //     glfwPollEvents();
-  // }
-
-  // glfwTerminate();
-
-
+  glfwTerminate();
 
 
   return 0;
-
 }
-
-
-
